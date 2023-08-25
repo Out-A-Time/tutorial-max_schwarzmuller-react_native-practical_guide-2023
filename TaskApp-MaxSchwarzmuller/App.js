32e512111c1df80635189855ff368c5acd441a0f
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-08-25 20:10:44 +0100

    Section 02.25-Scroll List vs FlatList

--- TaskApp-MaxSchwarzmuller/App.js
+++ TaskApp-MaxSchwarzmuller/App.js
@@ -1,14 +1,53 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function handleGoalInput(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function handleAddGoal() {
+    console.log("you are adding: ", enteredGoalText);
+    console.log(courseGoals);
+
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+    setEnteredGoalText("");
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!" />
-        <Button title="Add goal" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={enteredGoalText}
+          onChangeText={handleGoalInput}
+          placeholder="Add your task here"
+          style={styles.textInput}
+        />
+        <Button onPress={handleAddGoal} title="Add task" />
       </View>
-      <View>
-        <Text>List of goals....</Text>
+      <View style={styles.goalsContainer}>
+        <Text>List of tasks:</Text>
+        <ScrollView>
+          {courseGoals.map((goal) => (
+            <View key={goal} style={styles.goalItem}>
+              <Text style={styles.goalItemText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -16,6 +55,37 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    // marginRight: 2,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 5,
+    padding: 8,
+    backgroundColor: "#5e0acc",
+  },
+  goalItemText: {
+    color: "#fff",
   },
 });
