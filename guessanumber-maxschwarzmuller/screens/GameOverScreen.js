4061465fda17a8a8f1157f52b8e8e1274a23aecc
Out-A-Time <out-a-time@tutanota.com>
2023-10-04 21:27:17 +0100

    Chapter04.48-DONE.Creating empty skeleton of  GuessANumber game

---
+++ guessanumber-maxschwarzmuller/screens/GameOverScreen.js
@@ -0,0 +1,8 @@
+import { TextInput, View } from "react-native"
+
+function GameOverScreen(){
+    return <View>
+     <Text>This is game over screen</Text>
+    </View>
+}
+export default GameOverScreen
