e705e1cc4e3e87911f643e292c34724dce941e60
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-10-09 21:46:27 +0100

    Chapter04.50-Done. Styling for iOS/Android and styling number input element

--- guessanumber-maxschwarzmuller/screens/StartGameScreen.js
+++ guessanumber-maxschwarzmuller/screens/StartGameScreen.js
@@ -1,11 +1,39 @@
-import { TextInput, View } from "react-native"
+import { TextInput, View, StyleSheet } from "react-native"
 import PrimaryButton from "../components/PrimaryButton"
 
 function StartGameScreen(){
-    return <View>
-        <TextInput/>
+    return <View style={styles.inputContainer}>
+        <TextInput style={styles.numberInput} maxLength={2}/>
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>
 }
 export default StartGameScreen
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding:16,
+        backgroundColor: "#72063c",
+        borderRadius: 8,
+        // Android only
+        elevation: 5,
+        // iOS only
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomWidth:2,
+        borderBottomColor: '#ddb52f',
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
