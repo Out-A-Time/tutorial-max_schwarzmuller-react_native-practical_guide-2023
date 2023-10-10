311e94604ec1c98f25674116dba5c2c6726e1e64
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-10-10 22:17:34 +0100

    Chapter04.52 - done. Adding Visual Feedback to the custom buttons

--- guessanumber-maxschwarzmuller/components/PrimaryButton.js
+++ guessanumber-maxschwarzmuller/components/PrimaryButton.js
@@ -1,9 +1,44 @@
-import { Text, View } from "react-native"
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({children}){
-return <View>
-    <Text>{children}</Text>
-</View>
-
+function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed");
+  }
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressediOS]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
-export default PrimaryButton
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressediOS: {
+    opacity: 0.75,
+  },
+});
--- guessanumber-maxschwarzmuller/screens/StartGameScreen.js
+++ guessanumber-maxschwarzmuller/screens/StartGameScreen.js
@@ -1,39 +1,47 @@
-import { TextInput, View, StyleSheet } from "react-native"
-import PrimaryButton from "../components/PrimaryButton"
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen(){
-    return <View style={styles.inputContainer}>
-        <TextInput style={styles.numberInput} maxLength={2}/>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
+  );
 }
-export default StartGameScreen
+export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginTop: 100,
-        marginHorizontal: 24,
-        padding:16,
-        backgroundColor: "#72063c",
-        borderRadius: 8,
-        // Android only
-        elevation: 5,
-        // iOS only
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.25
-    },
-    numberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomWidth:2,
-        borderBottomColor: '#ddb52f',
-        color: '#ddb52f',
-        marginVertical: 8,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    }
-})
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    // Android only
+    elevation: 5,
+    // iOS only
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomWidth: 2,
+    borderBottomColor: "#ddb52f",
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
