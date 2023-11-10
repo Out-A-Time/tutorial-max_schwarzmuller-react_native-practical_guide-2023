1a2b100b335d38cd83eb0a50e70758aacd106f44
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-11-10 22:13:59 +0000

    Section04.69-done. Improving game visuals and using pseudo cascading styling as prop

--- guessanumber-maxschwarzmuller/screens/GameScreen.js
+++ guessanumber-maxschwarzmuller/screens/GameScreen.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
+import Card from "../components/ui/Card";
+import InstructionText from "../components/ui/InstructionText";
 
 let minBoundary = 1;
 let maxBoundary = 100;
@@ -56,17 +58,25 @@ const GameScreen = ({ userNumber, onGameOver }) => {
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <View>
-        <Text>Higher or Lower?</Text>
-        <View>
-          <PrimaryButton onPressButton={nextGuessHandler.bind(this, "higher")}>
-            +
-          </PrimaryButton>
-          <PrimaryButton onPressButton={nextGuessHandler.bind(this, "lower")}>
-            -
-          </PrimaryButton>
+      <Card>
+        <InstructionText styleProp={styles.instructionText}>
+          Higher or Lower?
+        </InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton
+              onPressButton={nextGuessHandler.bind(this, "higher")}
+            >
+              +
+            </PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPressButton={nextGuessHandler.bind(this, "lower")}>
+              -
+            </PrimaryButton>
+          </View>
         </View>
-      </View>
+      </Card>
       <View>
         <Text>Log Rounds</Text>
       </View>
@@ -80,5 +90,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 40,
+  },
+  instructionText: {
+    marginBottom: 12,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
